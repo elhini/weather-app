ea57d988e22178a8d33ec97c914bf04eb2987720
Artem Guslov <elhini@yandex-team.ru>
2019-11-08 01:38:51 +0300

    add github pages config and build folder

---
+++ build/precache-manifest.69b34cc6f94c058bfcd81c56c5844043.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc7df8be5b6de879d685dc5819dd8c49",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "7f7b846c56779ca4e66b",
+    "url": "/weather-app/static/css/main.f760b7a9.chunk.css"
+  },
+  {
+    "revision": "c91e68f0c919de5d6f73",
+    "url": "/weather-app/static/js/2.3b2b02c2.chunk.js"
+  },
+  {
+    "revision": "7f7b846c56779ca4e66b",
+    "url": "/weather-app/static/js/main.042c391f.chunk.js"
+  },
+  {
+    "revision": "2a772a3f304d502ff61c",
+    "url": "/weather-app/static/js/runtime-main.4addd464.js"
+  }
+]);
